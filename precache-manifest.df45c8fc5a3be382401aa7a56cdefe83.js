b8fa948f42ca2c892d603580099129db7d328962
Simon Wrang <simon.wrang2@gmail.com>
2018-10-27 19:12:23 +0200

    Updates

---
+++ precache-manifest.df45c8fc5a3be382401aa7a56cdefe83.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "26e966304117e3a79da5f6301f86b34c",
+    "url": "/project_v4/static/media/soda2.26e96630.jpg"
+  },
+  {
+    "revision": "03f1e7af7bc17206a7065c90335686b8",
+    "url": "/project_v4/static/media/mineral_water2.03f1e7af.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project_v4/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "577d8b39d9b092847873e0403b44573f",
+    "url": "/project_v4/static/media/coffee.577d8b39.png"
+  },
+  {
+    "revision": "40479643c8aae6e85718e508b98673cf",
+    "url": "/project_v4/static/media/black_tea.40479643.png"
+  },
+  {
+    "revision": "4a486436bcefe5f09870",
+    "url": "/project_v4/static/js/runtime~main.4a486436.js"
+  },
+  {
+    "revision": "8165dd0c8fd1026c3acd",
+    "url": "/project_v4/static/js/main.8165dd0c.chunk.js"
+  },
+  {
+    "revision": "8148ba707a37c60fab90",
+    "url": "/project_v4/static/js/1.8148ba70.chunk.js"
+  },
+  {
+    "revision": "8165dd0c8fd1026c3acd",
+    "url": "/project_v4/static/css/main.e10048a1.chunk.css"
+  },
+  {
+    "revision": "f46ebab4530774d9458a5ef29816ebbd",
+    "url": "/project_v4/index.html"
+  }
+];
